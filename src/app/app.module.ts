078fe2130f6ff65a03b9c6c2a40c2cb3da750d94
sandyrnelson <sandyrnelson@berkeley.edu>
2021-02-22 12:04:53 +0100

    Remove added structure to have a clean project start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -9,7 +8,6 @@ import { FormsModule } from '@angular/forms';
   ],
     imports: [
         BrowserModule,
-        FormsModule
     ],
   providers: [],
   bootstrap: [AppComponent]

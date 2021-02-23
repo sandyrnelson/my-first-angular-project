66d0c5baa9d56f890769be88cefd819614c5d89f
sandyrnelson <sandyrnelson@berkeley.edu>
2021-02-23 11:15:58 +0100

    Inject shopping list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import { Recipe } from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -9,7 +12,13 @@ export class RecipeService {
     new Recipe('Another Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'),
   ];
 
+  constructor(private slService: ShoppingListService) { }
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngrToShoppingList(ingredients: Ingredient[]){
+    this.slService.addIngredients(ingredients);
   }
 }

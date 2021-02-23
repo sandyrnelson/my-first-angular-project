33e2d6a8e60756553222f02200fff6bd6e975eff
sandyrnelson <sandyrnelson@berkeley.edu>
2021-02-23 10:21:19 +0100

    Add recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,15 @@
-export class RecipeService {
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
 
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }

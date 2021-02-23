fdd811ba704db2f00bde9d117505e257eae0d547
sandyrnelson <sandyrnelson@berkeley.edu>
2021-02-23 11:16:11 +0100

    Add test ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,8 +8,23 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'),
+    new Recipe('Hamburger',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Lettuce', 2),
+        new Ingredient('Tomatoes', 3),
+        new Ingredient('Buns', 2),
+      ]),
+    new Recipe('Salad',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872',
+      [
+        new Ingredient('Chicken', 1),
+        new Ingredient('Lettuce', 2),
+        new Ingredient('1000 island', 3),
+      ]),
   ];
 
   constructor(private slService: ShoppingListService) { }

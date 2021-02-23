930b0c32dddd6e0b70d845d3b2c77a7c3bb6ce8b
sandyrnelson <sandyrnelson@berkeley.edu>
2021-02-23 14:11:34 +0100

    Replace identifiers use built in form management from ngForms

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,18 +9,15 @@ import {ShoppingListService} from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputReference: ElementRef;
-  @ViewChild('amountInput') amountInputReference: ElementRef;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
 
-  onAddItem() {
-    const ingName = this.nameInputReference.nativeElement.value;
-    const ingAmount = this.amountInputReference.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddItem(form: NgForm) {
+    const value =  form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
   }
 }

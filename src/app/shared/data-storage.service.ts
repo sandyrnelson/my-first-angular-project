dd577378329e54f0f4364059ecfc92cb44e6a32b
sandyrnelson <sandyrnelson@berkeley.edu>
2021-03-07 17:51:37 +0100

    If ingredients are not included load with empty array

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Recipe} from '../recipes/recipe.model';
 import {RecipeService} from '../recipes/recipe.service';
+import {map} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -17,6 +18,11 @@ export class DataStorageService {
 
     fetchRecipes() {
       this.http.get<Recipe[]>('https://my-first-angular-df16e-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+        .pipe(map(recipes => {
+          return recipes.map(recipe => {
+            return {...recipe, ingredient: recipe.ingredients ? recipe.ingredients : []}
+          })
+        }))
         .subscribe( recipes => {
           this.recipeService.setRecipes(recipes)
         })

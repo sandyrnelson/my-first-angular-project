e2444b4f1a047d0e6d1552bffd5160bfbae18073
sandyrnelson <sandyrnelson@berkeley.edu>
2021-03-09 01:28:21 +0100

    Add background color to website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  constructor(private elementRef: ElementRef) {
+  }
+
+  ngAfterViewInit(): void {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#E8EAE1';
+  }
 }
